a9055ce45ec483702dc91ee8587f827d8c6b839a
Peter Hull <peterhull90@gmail.com>
2025-01-10 11:27:07 +0000

    This was generated by the templates/quick start guide

---
+++ windows/second/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::second::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::second::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
